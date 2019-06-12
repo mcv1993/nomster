8bed0da71030ee77346d7b78491baf63139bea21
mcv1993 <mcv1993@gmail.com>
2019-06-12 22:13:21 +0000

    font-test

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Nomster
     # the framework and any gems in your application.
   
     #Add the fonts path
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.paths << Rails.root.join("app","assets","fonts")
 
     #Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .tiff)   
